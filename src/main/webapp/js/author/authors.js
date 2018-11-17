b9df47dc942da9f2a9014df257206c5f102734d5
hexati <hexati@yahoo.com>
2018-11-17 14:49:01 +0200

    #18 added filtering on the front-end

--- src/main/webapp/js/author/authors.js
+++ src/main/webapp/js/author/authors.js
@@ -6,5 +6,24 @@ authorsModule.controller("AuthorsController", ["$http", "$scope", function ($htt
         .get(LISE_REST_URL + "authors")
         .then(function (response) {
             $scope.authors = response.data.authors;
-        })
+        });
+    searchBar.addEventListener("keydown", function (e) {
+        if (e.keyCode === 13) {
+            let url = LISE_REST_URL + "authors?";
+            if (searchBar.value !== "") {
+                let name = searchBar.value.split(" ");
+                if (name.length === 1)
+                    url += "fn=" + name[0];
+                else if (name.length === 2)
+                    url += "fn=" + name[0] + "&ln=" + name[1];
+                else if (name.length >= 3)
+                    url += "fn=" + name[0] + "&md=" + name[1] + "&ln=" + name[2];
+            }
+            $http
+                .get(url)
+                .then( (r) => {
+                    $scope.authors = r.data.authors;
+                });
+        }
+    })
 }]);
